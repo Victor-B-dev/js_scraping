a8b053ae35c83e5ee89b2cedb1ce370fc86e62a8
Victor <ban.victor728@gmail.com>
2024-11-14 15:49:06 -0500

    changed comment again regarding jquery object

--- cheerio/genres.js
+++ cheerio/genres.js
@@ -14,9 +14,10 @@ async function getGenres(){
     const $ = cheerio.load(html); 
     // typically the converted info is stored as $ because it resembles jQuery, an older framework for HTML document traversal.
     const genres = $('.side_categories li a').map((index, element) => {
-      return $(element).text().replace(/\s+/g, ' ').trim();
+      return $(element).text().replace(/\s+/g, ' ').trim(); // regex to remove whitespace chars
     }).get();
     // returns an array of all the genres available on the website's main page
+    // .get() converts the Jquery object (unintelligible list) into an regular JS array
 
     console.log('Genres:', genres);
   }
