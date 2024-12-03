f63a4a470fea6f366dfd9232e1b85d0c094d896a
Victor <ban.victor728@gmail.com>
2024-12-03 13:35:46 -0500

    added defaultViewport false & commented userDataDir - saving browser user data, some formatting

--- puppeteer/genre.js
+++ puppeteer/genre.js
@@ -1,10 +1,14 @@
 const puppeteer = require("puppeteer");
 
-const url = "https://books.toscrape.com/catalogue/category/books/science-fiction_16/index.html";
+const url = "https://books.toscrape.com/catalogue/category/books/science-fiction_16/index.html"; // scraping specifically the science fiction page
 
 async function getGenre(){
   try{
-    const browser = await puppeteer.launch({headless: false}); // launch browser w/out showing the GUI browser opening
+    const browser = await puppeteer.launch({
+      headless: false, // launch browser w/ GUI browser remaining open to see
+      defaultViewport: false, // by setting it to false, it loads the entire page, you can also set it to a specific value
+      // userDataDir:"./tmp" userDataDir allows you to specify a directory to store brower's user data - useful for cookies/local storage/cache etc - e.g. do not need to run successive logins each time code is ran which may trigger unintentional blocks
+    }); 
     const page = await browser.newPage(); // launch new tab
     await page.goto(url);
     await browser.close();
