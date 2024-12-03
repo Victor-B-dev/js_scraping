969dea8eef46381525c328535fb50c65f4f3cf62
Victor <ban.victor728@gmail.com>
2024-12-02 22:54:19 -0500

    added full basic screenshot functionality to code (sample) that will save in same directory. have another error with module but different instance shows teh code is fully functional

--- puppeteer/genre.js
+++ puppeteer/genre.js
@@ -4,9 +4,11 @@ const url = "https://books.toscrape.com/catalogue/category/books/science-fiction
 
 async function getGenre(){
   try{
-    const browser = puppeteer.launch({headless: false}); // launch browser
+    const browser = await puppeteer.launch({headless: false}); // launch browser
     const page = await browser.newPage(); // launch new tab
     await page.goto(url);
+    await page.screenshot({path: 'bookscrape.png'})
+    await browser.close();
 
   } catch (err){
     console.log(err);
