d06d3782fe6a0d64f1c051251f7effd53c1c792f
Victor <ban.victor728@gmail.com>
2024-12-02 22:33:20 -0500

    added browser launch, page launch, url launch

--- puppeteer/genre.js
+++ puppeteer/genre.js
@@ -2,6 +2,9 @@ const puppeteer = require("puppeteer");
 
 async function getGenre(){
   try{
+    const browser = puppeteer.launch({headless: false}); // launch browser
+    const page = await browser.newPage(); // launch new tab
+    await page.goto(url);
 
   } catch (err){
     console.log(err);
