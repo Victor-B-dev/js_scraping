f29b8746de4877fb431e8a7ce9813b55ba3cefc7
Victor <ban.victor728@gmail.com>
2024-12-03 12:52:48 -0500

    removed sample screenshot, removed screenshot function, set headless back to false for future debugging

--- puppeteer/genre.js
+++ puppeteer/genre.js
@@ -4,10 +4,9 @@ const url = "https://books.toscrape.com/catalogue/category/books/science-fiction
 
 async function getGenre(){
   try{
-    const browser = await puppeteer.launch({headless: true}); // launch browser w/out showing the GUI browser opening
+    const browser = await puppeteer.launch({headless: false}); // launch browser w/out showing the GUI browser opening
     const page = await browser.newPage(); // launch new tab
     await page.goto(url);
-    await page.screenshot({path: 'bookscrape.png'})
     await browser.close();
 
   } catch (err){
