b8418bbae41a55b933a342ad0e34a89f419e598b
Victor <ban.victor728@gmail.com>
2024-12-03 12:49:28 -0500

    change headless to true so chrome doesn't visually open

--- puppeteer/genre.js
+++ puppeteer/genre.js
@@ -4,7 +4,7 @@ const url = "https://books.toscrape.com/catalogue/category/books/science-fiction
 
 async function getGenre(){
   try{
-    const browser = await puppeteer.launch({headless: false}); // launch browser
+    const browser = await puppeteer.launch({headless: true}); // launch browser w/out showing the GUI browser opening
     const page = await browser.newPage(); // launch new tab
     await page.goto(url);
     await page.screenshot({path: 'bookscrape.png'})
